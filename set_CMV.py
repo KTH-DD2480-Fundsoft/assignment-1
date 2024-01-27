42d509fa13793257a6382db5ceb9ba2d13c82b65
Ludvig Skare <lskare@kth.se>
2024-01-27 19:05:43 +0100

    feat: add set_CMV_10 function and basic test cases
    Motivation:
    The set_CMV_10 function addresses and computes the LIC (Launch Interceptor Conditions) 10. Basic test cases handle incorrect input to the function along with evaluating correct computational performance
    
    Resolves: #23
    
    Authored by: Ludvig Skare lskare@kth.se

--- set_CMV.py
+++ set_CMV.py
@@ -1,7 +1,8 @@
+import numpy as np
 def set_CMV():
     """
         A list of the 15 Launch Interceptor Conditions (LIC)
-		
+        
 
         Parameters
         ----------
@@ -19,46 +20,89 @@ def set_CMV():
     pass
 
 def set_CMV_0():
-	pass
+    pass
 
 def set_CMV_1():
-	pass
+    pass
 
 def set_CMV_2():
-	pass
+    pass
 
 def set_CMV_3():
-	pass
+    pass
 
 def set_CMV_4():
-	pass
+    pass
 
 def set_CMV_5():
-	pass
+    pass
 
 def set_CMV_6():
-	pass
+    pass
 
 def set_CMV_7():
-	pass
+    pass
 
 def set_CMV_8():
-	pass
+    pass
 
 def set_CMV_9():
-	pass
+    pass
 
-def set_CMV_10():
-	pass
+def set_CMV_10(num_points, datapoints, parameters):
+    """
+        Set CMV_10 based on LIC 10
+
+        Parameters
+        ----------
+        num_points : (int)
+            Total number of data points
+        datapoints : List[Tuple[float, float]]
+            List of tuples 
+        parameters : (Dict)
+            Contains all the LIC and CMV parameters 
+        Returns
+        ----------
+        Bool
+            True if LIC 10 is fulfilled, else False
+    """
+    if num_points < 5:
+        return False
+    
+    epts  = parameters["epts"]
+    fpts  = parameters["fpts"]
+    area1 = parameters["area1"]
+    
+    assert (epts+fpts <= num_points-3), "Sum of E_PTS and F_PTS should at most be NUMPOINTS-3"
+
+    for i in range(num_points-epts-fpts-2):
+        # The area of the triangle between the three data points is computed using a generalized cross product of 2D vectors
+        # Vectors a and b are calculated by finding the distance between the different points
+        # Both vectors originate from the same origin point (point 1) permitting the cross product operation
+        # Components of vector a which is the difference between point 2 and point 1. 
+        a_x = datapoints[i+epts+1][0] - datapoints[i][0] 
+        a_y = datapoints[i+epts+1][1] - datapoints[i][1]
+        # Components of vector b which is the difference between point 3 and point 1
+        b_x = datapoints[i+epts+fpts+2][0] - datapoints[i][0]
+        b_y = datapoints[i+epts+fpts+2][1] - datapoints[i][1]
+        # Simplified cross product for 2D vectors 
+        area_of_triangle = np.abs(0.5*(a_x*b_y - a_y*b_x))
+
+        if area_of_triangle > area1:
+            return True
+
+    return False 
+
+    
 
 def set_CMV_11():
-	pass
+    pass
 
 def set_CMV_12():
-	pass
+    pass
 
 def set_CMV_13():
-	pass
+    pass
 
 def set_CMV_14():
-	pass
+    pass
--- test_decide.py
+++ test_decide.py
@@ -1,8 +1,44 @@
 import unittest
+from set_CMV import *
 
 class TestDecide(unittest.TestCase):
     def test_cmv_0(self):
         pass
+
+
+    def test_cmv_10(self):
+        # Define test parameters (should probably be moved to JSON test file later)
+
+        parameters = {}
+        parameters["epts"] = 2
+        parameters["fpts"] = 3
+        parameters["area1"] = 1        
+        num_points = 10
+        num_points_less = 6
+
+        datapoints_0 = [(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0)]
+        datapoints_1 = [(0,0),(0,0),(0,0),(2,0),(0,0),(2,0),(0,0),(0,2),(0,0),(0,2)]
+        datapoints_2 = [(1,2),(0,0),(0,0),(1,3),(0,0),(0,0),(0,0),(2,2),(0,0),(0,0)]
+        datapoints_3 = [(0,0),(-2,-3),(0,0),(0,0),(-5,1),(0,0),(0,0),(0,0),(1,2),(0,0)]
+
+        #def set_CMV_10(num_points, datapoints, parameters):
+
+        # Test computational logic in set_CMV_10 function
+        self.assertFalse(set_CMV_10(num_points, datapoints_0, parameters))
+        self.assertFalse(set_CMV_10(num_points, datapoints_2, parameters))
+        self.assertTrue(set_CMV_10( num_points, datapoints_1, parameters))
+        self.assertTrue(set_CMV_10( num_points, datapoints_3, parameters))
+
+        # Change AREA1 to test comparative computations
+        parameters["area1"] = 100
+        self.assertFalse(set_CMV_10(num_points, datapoints_1, parameters))
+        self.assertFalse(set_CMV_10(num_points, datapoints_3, parameters))
+
+        # Test built-in assertions in set_CMV_10 function
+        with self.assertRaises(AssertionError):   # Tests that NUMPOINTS-3 is larger than sum of E_PTS and F_PTS
+            set_CMV_10(num_points_less, datapoints_0, parameters)
+ 
+
 
 if __name__ == '__main__':
     unittest.main()
