4a5782dc871c2185ddb75c1a2e4a0833ebf8c332
Ludvig Skare <lskare@kth.se>
2024-01-27 14:28:26 +0100

    feat : add CMV_0 function and basic test cases
    Motivation:
    The CMV_0 function addresses the LIC (Launch Interceptor Conditions) 0. Basic test cases handle incorrect input to the function along with evaluating correct computational performance
    
    Resolves: #5
    
    Authored by: Ludvig Skare lskare@kth.se

--- set_CMV.py
+++ set_CMV.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def set_CMV():
     """
         A list of the 15 Launch Interceptor Conditions (LIC)
@@ -18,8 +20,28 @@ def set_CMV():
     set_CMV_0()
     pass
 
-def set_CMV_0():
-	pass
+def set_CMV_0(LENGTH1, x, y):
+    """
+        Set CMV_0 based on LIC 1
+		
+		Parameters
+        ----------
+        LENGTH1 : (int/float)
+            Maximum length between a set of two consequative points.
+        x       : (list of ints/floats)
+            List of x coordinates in the trajectory plane
+		y       : (list of ints/floats)
+            List of y coordinates in the trajectory plane
+
+        Returns Bool depending on if LIC 1 is fulfilled
+	"""
+    assert (len(x)==len(y)), "Coordinate vectors x and y should be of same length"
+    assert (isinstance(LENGTH1, (float, int)) and LENGTH1 >= 0), "Parameter LENGTH1 should be and int or float with value greater or equal to 0"
+	
+    for i in range(len(x)-1):
+        if (np.sqrt(np.square(x[i+1]-x[i])+np.square(y[i+1]-y[i])) > LENGTH1):
+            return True	
+    return False
 
 def set_CMV_1():
 	pass
--- test_decide.py
+++ test_decide.py
@@ -1,8 +1,31 @@
 import unittest
+from set_CMV import *
+
 
 class TestDecide(unittest.TestCase):
     def test_cmv_0(self):
-        pass
+        # Define test parameters (should probably be moved to JSON test file later)
+        LENGTH1_1 = 2
+        LENGTH1_2 = "2"
+        x_1 = [0,1,2,3,4]
+        y_1 = [0,1,2,3,4]
+        x_2 = [0,3,0,0,0]
+        y_2 = [0,4,0,0,0]
+        x_3 = [-1,-3,0,1,3]
+        y_3 = [0,0,0,0,0]
+        x_short = [0]
+
+        # Test computational logic in set_CMV_0 function
+        self.assertFalse(set_CMV_0(LENGTH1_1, x_1, y_1), "CMV_0: Maximum distance is smaller than parameter LENGTH1")
+        self.assertTrue(set_CMV_0(LENGTH1_1, x_2, y_2), "CMV_0: Maximum distance is larger than parameter LENGTH1")
+        self.assertTrue(set_CMV_0(LENGTH1_1, x_3, y_3), "CMV_0: Maximum distance is larger than parameter LENGTH1")
+
+        # Test built-in assertions in set_CMV_0 function
+        with self.assertRaises(AssertionError):
+            set_CMV_0(LENGTH1_2,x_1,x_2)
+        with self.assertRaises(AssertionError):
+            set_CMV_0(LENGTH1_1,x_short,x_2)
+
 
 if __name__ == '__main__':
     unittest.main()
