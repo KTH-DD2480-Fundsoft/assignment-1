fe539730d5905a5c057b1b5ce9f5c60ff02c6be1
Ludvig Skare <lskare@kth.se>
2024-01-27 16:41:09 +0100

    feat: add set_CMV_11 function and basic test cases
    Motivation:
    The set_CMV_11 function addresses and computes the LIC (Launch INterceptor Conditions) 11. Basic test cases handle incorrect input to the function along with evaluating correct computation performance
    
    Resolves: #25
    
    Authored by: Ludvig Skare lskare@kth.se

--- set_CMV.py
+++ set_CMV.py
@@ -51,8 +51,34 @@ def set_CMV_9():
 def set_CMV_10():
 	pass
 
-def set_CMV_11():
-	pass
+def set_CMV_11(G_PTS, NUMPOINTS, x, y):
+	"""
+        Set CMV_11 based on LIC 11
+		
+		Parameters
+        ----------
+        G_PTS : (int)
+            Number of consecutive points between the two datapoints x[i] and x[j].
+		NUMPOINTS : (int)
+			Number of datapoints. Equal to the length of the data vectors x and y
+        x       : (list of ints/floats)
+            List of x coordinates in the trajectory plane
+		y       : (list of ints/floats)
+            List of y coordinates in the trajectory plane
+
+        Returns Bool depending on if LIC 11 is fulfilled
+	"""
+	if NUMPOINTS < 3:
+		return False
+	assert (isinstance(G_PTS, int)), "G_PTS needs to be an int"
+	assert (isinstance(NUMPOINTS, int)), "NUMPOINTS needs to be an int"
+	assert (1<= G_PTS and G_PTS <= NUMPOINTS-2), "CMV_11: G_PTS value not between 1 and NUMPOINTS-2"
+	assert (len(x) == NUMPOINTS), "length of x vector should be equal to number of data points (NUMPOINTS)"
+	
+	for i in range(NUMPOINTS-G_PTS-1):
+		if x[i+G_PTS+1]-x[i] < 0:
+			return True
+	return False
 
 def set_CMV_12():
 	pass
--- test_decide.py
+++ test_decide.py
@@ -1,8 +1,41 @@
 import unittest
+from set_CMV import *
 
 class TestDecide(unittest.TestCase):
     def test_cmv_0(self):
         pass
+
+    def test_cmv_11(self):
+        # Define test parameters (should probably be moved to JSON test file later)
+        G_PTS = 4
+        NUMPOINTS = 10
+        NUMPOINTS_less = 1
+        y   = [0,0,0,0,0,0,0,0,0,0]
+        x_1 = [0,1,2,3,4,5,6,7,8,9]
+        x_2 = [0,3,5,6,1,2,8,9,10,0]
+        x_3 = [-1,-2,-3,0,1,2,0,0,0,0]
+        
+
+        x_short = [0]
+
+        # Test computational logic in set_CMV_11 function
+        self.assertFalse(set_CMV_11(G_PTS,NUMPOINTS,x_1,y), "test_cmv_11: x vector is only increasing")
+        self.assertTrue(set_CMV_11(G_PTS,NUMPOINTS,x_2,y), "test_cmv_11: x vector includes correct set of datapoints")
+        self.assertTrue(set_CMV_11(G_PTS,NUMPOINTS,x_3,y), "test_cmv_11: x vector includes correct set of datapoints")
+        self.assertFalse(set_CMV_11(G_PTS,NUMPOINTS_less,x_2,y), "test_cmv_11: NUMPOINTS less than 3")
+
+        # Test built-in assertions in set_CMV_0 function
+        with self.assertRaises(AssertionError):   # Tests that length of x vector equal to NUMPOINTS int
+            set_CMV_11(G_PTS,NUMPOINTS,x_short,y)
+        G_PTS_str = "4"
+        with self.assertRaises(AssertionError):   # Tests that G_PTS variable is an int
+            set_CMV_11(G_PTS_str,NUMPOINTS,x_short,y)
+        NUMPOINTS_str = "10"
+        with self.assertRaises(TypeError):        # Tests that NUMPOINTS variable is an int
+            set_CMV_11(G_PTS,NUMPOINTS_str,x_short,y)
+        with self.assertRaises(AssertionError):   # Tests that G_PTS is between 1 and NUMPOINTS-2
+            set_CMV_11(G_PTS,NUMPOINTS,x_short,y)
+
 
 if __name__ == '__main__':
     unittest.main()
