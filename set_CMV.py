66b8bfe8177ca6603b90cd24b3a21b9f807e7018
Victor Stenmark <victor.stenmark22@gmail.com>
2024-01-28 19:36:53 +0100

    feat: add functionality and test for set_cmv_1
    Motivation: set_cmv_1 determines LIC with index 1. This commit has the
    functionality for set_cmv_1 in addition to a basic unit test.
    
    Resolves #6
    Co-authored-by: Victor Stenmark <Victor.stenmark22@gmail.com>

--- set_CMV.py
+++ set_CMV.py
@@ -21,8 +21,70 @@ def set_CMV():
 def set_CMV_0():
 	pass
 
-def set_CMV_1():
-	pass
+def set_CMV_1(numpoints, datapoints, parameters):
+	"""
+	Set CMV_1 based on LIC 1
+	
+	Parameters
+	----------
+	num_points : (int)
+		Total number of data points
+	datapoints : List[Tuple[float, float]]
+		List of tuples 
+	parameters : (Dict)
+		Contains all the LIC and CMV parameters 
+		
+	Returns 
+	----------
+	Bool 
+		True if LIC 1 is fulfilled, else False    
+    """
+	radius_cond = False
+	radius = parameters["radius1"]
+
+	for i in range(numpoints - 2):
+		p_1 = complex(datapoints[i][0], datapoints[i][1])
+		p_2 = complex(datapoints[i+1][0], datapoints[i+1][1])
+		p_3 = complex(datapoints[i+2][0], datapoints[i+2][1])
+
+		# We can always encompass a single point
+		if p_1 == p_2 == p_3:
+			continue
+
+		# If two points are equal, then check if the distance between
+		# the differing points is greater than the diameter. 
+		if p_1 == p_2:
+			if abs(p_3 - p_2) > 2*radius:
+				radius_cond = True
+			else:
+				continue
+		elif p_2 == p_3:
+			if abs(p_1 - p_3) > 2*radius:
+				radius_cond = True
+			else:
+				continue
+		elif p_3 == p_1:
+			if abs(p_2 - p_1) > 2*radius:
+				radius_cond = True
+			else:
+				continue
+		if radius_cond:
+			break
+
+		# Code taken from https://math.stackexchange.com/questions/213658/get-the-equation-of-a-circle-when-given-3-points
+		w = (p_3 - p_1)/(p_2 - p_1)
+
+		# If the points are collinear, then they cannot create a circle
+		if abs(w.imag) <= 0.0001:
+			continue
+
+		c = (p_2 - p_1)*(w - abs(w)**2)/(2j*w.imag) + p_1  
+		circumradius = abs(p_1 - c)
+
+		if circumradius > radius:
+			radius_cond = True
+			break
+	return radius_cond
 
 def set_CMV_2():
 	pass
--- test_decide.py
+++ test_decide.py
@@ -1,8 +1,48 @@
 import unittest
+from set_CMV import *
 
 class TestDecide(unittest.TestCase):
     def test_cmv_0(self):
         pass
+
+    def test_cmv_1_equal_points(self):
+        """
+        Given a set of three points where two are equal and where the
+        euclidean distance between the differing points is 2, 
+        the points can be encompassed by a circle of radius 1.
+        """
+        parameters = {}
+        parameters["radius1"] = 1
+        datapoints = [(0.0, 0.0), (0.0, 0.0), (0.0, 2.0)]
+        num_points = len(datapoints)
+        self.assertFalse(set_CMV_1(num_points, datapoints, parameters))
+
+    def test_cmv_1_collinear(self):
+        """
+        Given a set of three collinear points, the points should not 
+        be able to be contained by a circle.
+        """
+        parameters = {}
+        parameters["radius1"] = 0.5
+        datapoints = [(1.0, 1.0), (2.0, 2.0), (3.0, 3.0)]
+        num_points = len(datapoints)
+        self.assertFalse(set_CMV_1(num_points, datapoints, parameters))
+
+    def test_cmv_1(self):
+        """
+        The points (1, 0), (-1, 0), and (0, 1) should be contained a by a circle
+        with radius 1. set_CMV_1 should therefore be true with RADIUS set to 0.5
+        but not when RADIUS is set to 1. 
+        """
+        parameters = {}
+        parameters["radius1"] = 0.5
+        datapoints = [(0.0, 0.0), (1.0, 0.0), (-1.0, 0.0), (0.0, 1.0), (0.0, 0.0)]
+        num_points = len(datapoints)
+
+        self.assertTrue(set_CMV_1(num_points, datapoints, parameters))
+        parameters["radius1"] = 1
+        self.assertFalse(set_CMV_1(num_points, datapoints, parameters))
+
 
 if __name__ == '__main__':
     unittest.main()
