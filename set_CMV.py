83ec33386ef1cc7e52141fbc2872574ffcfe2208
rasdan <rasdan@kth.se>
2024-01-29 11:46:12 +0100

    feat: add set_CMV_3 function and basic test cases
    add function set_CMV_3 which checks data_points for any three
    consecutive points which form a triangle of area > AREA1
    add function test_cmv_3 which tests two positive and two negative
    instances on set_CMV_3.

--- set_CMV.py
+++ set_CMV.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def set_CMV():
     """
         A list of the 15 Launch Interceptor Conditions (LIC)
@@ -27,8 +29,19 @@ def set_CMV_1():
 def set_CMV_2():
 	pass
 
-def set_CMV_3():
-	pass
+def set_CMV_3(num_points, data_points, parameters):
+    ''' Returns true iff there exists three consecutive
+        points in 'data_points' that form a triangle 
+        of size greater than parameters["AREA1"]. '''
+
+    area = parameters["AREA1"]
+    for i in range(num_points-2):
+        v1,v2,v3 = ( np.array(data_points[i])
+                   , np.array(data_points[i+1])
+                   , np.array(data_points[i+2]) ) 
+        a = np.abs(np.cross(v2-v1,v3-v1))/2
+        if a > area: return True
+    return False
 
 def set_CMV_4():
 	pass
--- test_decide.py
+++ test_decide.py
@@ -1,8 +1,91 @@
+import set_CMV
 import unittest
 
 class TestDecide(unittest.TestCase):
     def test_cmv_0(self):
         pass
+    
+
+    def test_cmv_3(self): 
+        ''' Tests the function set_CMV_3 which calculates the 
+            LIC3 condition. This function passes two false instances
+            (where there exists no triangle in any consecutive data points 
+            of area greater than AREA1) and two true instances (where 
+            there exists a triangle in consecutive datapoints where the 
+            area is greater than AREA1).'''
+
+        num_points = 9
+        
+        # TRUE case
+        parameters_true1 = { 
+            "AREA1" : 7 
+        } 
+
+        data_points_true1 = [ 
+            (-1.0,-1.0),(3.0,3.0), (3.0,-1.0), 
+            (0.0,-1.0), (1.0,-1.0), (2.0,-1.0), 
+            (2.0,2.0), (1.0,1.0), (0.0,0.0),
+        ]
+        # TRUE case
+        parameters_true2 = { 
+            "AREA1" : 40 
+        } 
+
+        data_points_true2 = [ 
+            (0.0,0.0), (10.0,10.0), (10.0,0.0), 
+            (-1.0,1.0), (0.0,1.0), (1.0,1.0), 
+            (-1.0,-1.0), (0.0,-1.0), (1.0,-1.0) 
+        ]
+        # FALSE case
+        parameters_false1 = { 
+            "AREA1" : 7 
+        } 
+
+        data_points_false1 = [ 
+            (-1.0,1.0), (0.0,1.0), (1.0,1.0), 
+            (-1.0,0.0), (0.0,0.0), (1.0,0.0), 
+            (-1.0,-1.0), (0.0,-1.0), (1.0,-1.0) 
+        ]
+        # FALSE case
+        parameters_false2 = { 
+            "AREA1" : 10 
+        } 
+
+        data_points_false2 = [ 
+            (-1.0,-1.0), (1.0,1.0), (0.0,0.0), 
+            (0.0,-1.0), (1.0,-1.0), (2.0,-1.0), 
+            (2.0,2.0), (3.0,3.0), (3.0,-1.0) 
+        ]
+
+        self.assertTrue(
+            set_CMV.set_CMV_3(
+                num_points,
+                data_points_true1,
+                parameters_true1 
+            )
+        )
+        self.assertTrue(
+            set_CMV.set_CMV_3(
+                num_points,
+                data_points_true2,
+                parameters_true2 
+            )
+        )
+        self.assertFalse(
+            set_CMV.set_CMV_3(
+                num_points,
+                data_points_false1,
+                parameters_false1 
+            )
+        )
+        self.assertFalse(
+            set_CMV.set_CMV_3(
+                num_points,
+                data_points_false2,
+                parameters_false2 
+            )
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
