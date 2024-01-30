92115f89520cad3872fb3b8f499adfeb92985b4e
sebastianmonten <94540990+sebastianmonten@users.noreply.github>
2024-01-30 10:55:44 +0100

    fix: comment back in unittests for CMV logic 0 and 11

--- test_decide.py
+++ test_decide.py
@@ -3,44 +3,44 @@ from set_CMV import *
 
 
 class TestDecide(unittest.TestCase):
-    # def test_cmv_0(self):
-    #     # Define test parameters (should probably be moved to JSON test file later)
-    #     parameters = {}
-    #     parameters["length1"] = 2
+    def test_cmv_0(self):
+        # Define test parameters (should probably be moved to JSON test file later)
+        parameters = {}
+        parameters["length1"] = 2
 
-    #     num_points = 5
+        num_points = 5
 
-    #     datapoints_1 = [(0,0),(1,1),(2,2),(3,3),(4,4)]
-    #     datapoints_2 = [(0,0),(3,4),(0,0),(0,0),(0,0)]
-    #     datapoints_3 = [(-1,0),(-3,0),(0,0),(1,0),(3,0)]
+        datapoints_1 = [(0,0),(1,1),(2,2),(3,3),(4,4)]
+        datapoints_2 = [(0,0),(3,4),(0,0),(0,0),(0,0)]
+        datapoints_3 = [(-1,0),(-3,0),(0,0),(1,0),(3,0)]
 
-    #     # Test computational logic in set_CMV_0 function
-    #     self.assertFalse(set_CMV_0(num_points, datapoints_1, parameters), "CMV_0: Maximum distance is smaller than parameter LENGTH1")
-    #     self.assertTrue(set_CMV_0(num_points, datapoints_2, parameters), "CMV_0: Maximum distance is larger than parameter LENGTH1")
-    #     self.assertTrue(set_CMV_0(num_points, datapoints_3, parameters), "CMV_0: Maximum distance is larger than parameter LENGTH1")
+        # Test computational logic in set_CMV_0 function
+        self.assertFalse(set_CMV_0(num_points, datapoints_1, parameters), "CMV_0: Maximum distance is smaller than parameter LENGTH1")
+        self.assertTrue(set_CMV_0(num_points, datapoints_2, parameters), "CMV_0: Maximum distance is larger than parameter LENGTH1")
+        self.assertTrue(set_CMV_0(num_points, datapoints_3, parameters), "CMV_0: Maximum distance is larger than parameter LENGTH1")
     
 
-    # def test_cmv_11(self):
-    #     # Define test parameters (should probably be moved to JSON test file later)
-    #     parameters = {
-    #         "gpts" : 4
-    #     }
+    def test_cmv_11(self):
+        # Define test parameters (should probably be moved to JSON test file later)
+        parameters = {
+            "gpts" : 4
+        }
 
-    #     num_points = 10
-    #     num_points_less = 1
+        num_points = 10
+        num_points_less = 1
 
-    #     datapoints_1 = [(0,0),(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),(8,0),(9,0)]
-    #     datapoints_2 = [(0,0),(3,0),(5,0),(6,0),(1,0),(2,0),(8,0),(9,0),(10,0),(0,0)]
-    #     datapoints_3 = [(-1,0),(-2,0),(-3,0),(0,0),(1,0),(2,0),(0,0),(0,0),(0,0),(0,0)]
+        datapoints_1 = [(0,0),(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),(8,0),(9,0)]
+        datapoints_2 = [(0,0),(3,0),(5,0),(6,0),(1,0),(2,0),(8,0),(9,0),(10,0),(0,0)]
+        datapoints_3 = [(-1,0),(-2,0),(-3,0),(0,0),(1,0),(2,0),(0,0),(0,0),(0,0),(0,0)]
 
-    #     # Test computational logic in set_CMV_11 function
-    #     self.assertFalse(set_CMV_11(num_points, datapoints_1, parameters), "test_cmv_11: x vector is only increasing")
-    #     self.assertTrue(set_CMV_11( num_points, datapoints_2, parameters), "test_cmv_11: x vector includes correct set of datapoints")
-    #     self.assertTrue(set_CMV_11( num_points, datapoints_3, parameters), "test_cmv_11: x vector includes correct set of datapoints")
-    #     self.assertFalse(set_CMV_11(num_points_less, datapoints_2, parameters), "test_cmv_11: NUMPOINTS less than 3")
+        # Test computational logic in set_CMV_11 function
+        self.assertFalse(set_CMV_11(num_points, datapoints_1, parameters), "test_cmv_11: x vector is only increasing")
+        self.assertTrue(set_CMV_11( num_points, datapoints_2, parameters), "test_cmv_11: x vector includes correct set of datapoints")
+        self.assertTrue(set_CMV_11( num_points, datapoints_3, parameters), "test_cmv_11: x vector includes correct set of datapoints")
+        self.assertFalse(set_CMV_11(num_points_less, datapoints_2, parameters), "test_cmv_11: NUMPOINTS less than 3")
 
     def test_cmv_5(self):
-        # Define test parameters (should probably be moved to JSON test file later)
+        # Define test parameters
         parameters = {}
 
         num_points = 5
@@ -50,7 +50,7 @@ class TestDecide(unittest.TestCase):
         datapoints_3 = [(-1,0),(-3,0),(0,0),(1,0),(3,0)]
         datapoints_4 = [(-1,0),(-0,0),(0,0),(1,0),(3,0)]
 
-        # Test computational logic in set_CMV_0 function
+        # Test computational logic in set_CMV_5 function
         self.assertFalse(set_CMV_5(num_points, datapoints_1, parameters), "CMV_5: No X[i] > X[i+1]")
         self.assertTrue(set_CMV_5(num_points, datapoints_2, parameters), "CMV_5: Last two points satisifes X[i] > X[i+1]")
         self.assertTrue(set_CMV_5(num_points, datapoints_3, parameters), "CMV_5: Points with negative x-values satisifes X[i] > X[i+1]")
