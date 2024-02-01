a674f06a2f0d4f085c2d9b9ae72a62263771f40e
Stenmarken <67392286+Stenmarken@users.noreply.github.com>
2024-02-01 22:13:13 +0100

    Feature/63/second system test (#78)
    * WIP: Add system test 1
    
    * test: Create system test for all true LIC
    
    Implement a system test where every LIC condition is true. That means
    that the entire PUM and FUV matrices are true and also launch condition.
    
    Resolves #63

--- test_system.py
+++ test_system.py
@@ -1,6 +1,6 @@
 import numpy as np
 import unittest
-from set_CMV import *
+from LIC_evaluation import *
 from decide import *
 
 ####################################################################################################################
@@ -164,6 +164,111 @@ parsed_input_3 = {
             ,False,True ,False]
 }
 
+# System Test 4
+# Given inputs
+parsed_input_4 = {
+    "numpoints" : 53,
+    "datapoints" : [
+    [0, 0], # CMV 0, CMV 1, CMV 11
+    [3, 3], # CMV 0, CMV 1, CMV 2
+    [10, 10], # CMV 1, CMV 2, CMV 11
+    [2, 1], # CMV 2
+    [0, 0], # CMV 3, CMV 14
+    [0, 6], # CMV 3
+    [3, 3], # CMV 3
+    [1, 1], # CMV 4
+    [0, 0], # CMV 5
+    [2, 1], # CMV 5
+    [0, 0], # CMV 6
+    [0, 2], # CMV 6
+    [0, 0], # CMV 6
+    [1, 1], # CMV 7
+    [1.5, 1.5], # CMV 7
+    [5, 5], # CMV 7
+    [0, 0], # CMV 8
+    [2, 2], # CMV 8
+    [4, 4], # CMV 8
+    [2, 2], # CMV 8
+    [10, 5], # CMV 8
+    [2, 2], # CMV 9
+    [0, 0], # CMV 9
+    [10, 5], # CMV 9
+    [0, 0], # CMV 9
+    [0, 0], # CMV 9
+    [5, 10], # CMV 9
+    [1, 1], # CMV 10
+    [5, 2], # CMV 10
+    [7, 0], # CMV 10
+    [11, 7], # CMV 10
+    [13, 2], # CMV 10
+    [0, 2], # CMV 10
+    [1, 0], # CMV 10, CMV 12
+    [2, 13], # CMV 10
+    [4, 0], # CMV 12
+    [0, 3], # CMV 13
+    [0, 0], # CMV 13
+    [1, 0], # CMV 13
+    [0, 0], # CMV 13
+    [5, 5], # CMV 13
+    [2, 2], # CMV 14
+    [0, 0], # CMV 14
+    [1, 1], # CMV 14
+    [6, 2], # CMV 14
+    [0, 0], # CMV 14
+    [1, 1], # CMV 14
+    [50, 1], # CMV 14
+    [4, 10], # CMV 14
+    [1, 1], # CMV 4
+    [1, -1], # CMV 4
+    [-1, -1], # CMV 4
+    [-1, 1], # CMV 4
+    ],
+
+    "parameters" : {
+        "length1" : 2,
+        "length2" : 4,
+        "radius1" : 2,
+        "radius2" : 20,
+        "epsilon" : 0.5,
+        "area1" : 1,
+        "area2" : 100,
+        "qpts" : 2,
+        "quads" : 1,
+        "npts" : 3,
+        "dist" : 1,
+        "kpts" : 1,
+        "apts" : 1,
+        "bpts" : 1,
+        "cpts" : 1,
+        "dpts" : 2,
+        "epts" : 2,
+        "fpts" : 3,
+        "gpts" : 1,
+        "kpts" : 1
+    },
+    "LCM" : [ ["ANDD"   ,"ANDD"   ,"ANDD"    ,"AND"    ,"ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD"]
+            , ["ANDD"   ,"ANDD"   ,"ANDD"    ,"ANDD"    ,"ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD"]
+            , ["ANDD"    ,"ANDD"    ,"ANDD"   ,"ANDD"   ,"ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD"]
+            , ["ANDD"   ,"ANDD"    ,"ANDD"   ,"ANDD"   ,"ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD"]
+            , ["ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD"]
+            , ["ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD"]
+            , ["ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD"]
+            , ["ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD"]
+            , ["ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD"]
+            , ["ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD"]
+            , ["ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD"]
+            , ["ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD"]
+            , ["ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD"]
+            , ["ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD"]
+            , ["ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD","ANDD"] ],
+    
+    "PUV" : [True, True, True
+            ,True,True , True
+            ,True,True ,True
+            ,True ,True, True
+            ,True,True ,True]
+}
+
 ####################################################################################################################
 # CORRECT VALUES FOR TEST 1 THAT ACT AS COMPUTATIONAL CHECKPOINTS
 CMV_correct_test_1 = [ False, True , True , True , False
@@ -273,6 +378,13 @@ parameters_test_3   = parsed_input_3["parameters"]
 lcm_test_3          = parsed_input_3["LCM"]
 puv_test_3          = parsed_input_3["PUV"]
 
+# Test 4 inputs
+num_points_test_4   = parsed_input_4["numpoints"]
+data_points_test_4  = [np.array(p) for p in parsed_input_4["datapoints"]]
+parameters_test_4   = parsed_input_4["parameters"]
+lcm_test_4          = parsed_input_4["LCM"]
+puv_test_4          = parsed_input_4["PUV"]
+
 calculated_cmv = [ False, True , True , True , False
             ,False, False, False, False, False
             ,False, False, False, False, False]
@@ -340,8 +452,21 @@ class TestDecide(unittest.TestCase):
         calculated_LAUNCH = compute_LAUNCH(calculated_FUV)
         self.assertEqual(calculated_LAUNCH, LAUNCH_correct_test_3), "test_3: Calculated LAUNCH not the same as the correct test_3 version"
 
+    def test_system_only_true_LIC(self):  
+        """ 
+        Tests a set of data points which should satisfy every LIC condition, 
+        where the LCM only has ANDD values, and the PUV has true values. This should
+        result in all true values in the CMV, PUM, FUV and launch.
+        """      
+        calculated_cmv = compute_cmv(parameters_test_4, num_points_test_4, data_points_test_4)
+        calculated_PUM = compute_PUM(lcm_test_4, calculated_cmv, lcm_test_4)
+        calculated_FUV = compute_FUV(calculated_PUM, puv_test_4)
+        calculated_launch = compute_LAUNCH(calculated_FUV)
 
-
+        self.assertTrue(all(calculated_cmv)) # Ensure that all LICs are true
+        self.assertTrue(all([x for xs in calculated_PUM for x in xs])) # Ensure that all values are true in the PUM
+        self.assertTrue(all(calculated_FUV))
+        self.assertTrue(calculated_launch)
 
 
 if __name__ == '__main__':
