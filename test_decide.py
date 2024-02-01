d53a41e889257329ce8210b2f893951a903a396d
Ludvig Skare <lskare@kth.se>
2024-02-01 11:28:46 +0100

    feat : add functionality and tests for CMV 13 along with helper function
    Motivation:
    The function set_cmv_13() calculates and returns the value for LIC 2. The tests assess set_cmv_13()s computational correctness. The helper function smallest_containing_circle() improves code efficiency and complexity by being called upon several times. Hopefully we can do a refactor over the launch conditions later so that more set_cmv() functions can use this helper function thus making the code more concise and effective.
    
    Resolves #27

--- test_decide.py
+++ test_decide.py
@@ -64,6 +64,7 @@ class TestDecide(unittest.TestCase):
         datapoints_2 = [(0,0),(3,0),(5,0),(6,0),(1,0),(2,0),(8,0),(9,0),(10,0),(0,0)]
         datapoints_3 = [(-1,0),(-2,0),(-3,0),(0,0),(1,0),(2,0),(0,0),(0,0),(0,0),(0,0)]
 
+
         # Test computational logic in set_CMV_11 function
         self.assertFalse(set_CMV_11(num_points, datapoints_1, parameters), "test_cmv_11: x vector is only increasing")
         self.assertTrue(set_CMV_11( num_points, datapoints_2, parameters), "test_cmv_11: x vector includes correct set of datapoints")
@@ -71,8 +72,74 @@ class TestDecide(unittest.TestCase):
         self.assertFalse(set_CMV_11(num_points_less, datapoints_2, parameters), "test_cmv_11: NUMPOINTS less than 3")
 
     
- 
+    def test_cmv_13(self):
+
+        num_points = 10
+        parameters = {
+            "APTS" : 2,
+            "BPTS" : 3,
+            "RADIUS1" : 1,
+            "RADIUS2" : 2
+        }
+
+        datapoints_1 = [(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0)]
+        datapoints_2 = [(0,0),(1.4,1.4),(1,1),(1,1),(1.5,0),(0,0),(0,0),(0,1),(0,-1.9),(1,1)]
+        datapoints_3 = [(1.4,1.4),(0,0),(0,0),(1.4,1.4),(0,0),(0,0),(0,0),(-1.1,-1.1),(0,0),(0,0)]
+        datapoints_4 = [(-1.5,0),(0,0),(0,0),(0,0),(5,5),(5,5),(0,0),(3,0),(0,0),(0,0)]
 
 
+        # Test computational logic in set_CMV_13 function
+        self.assertFalse(set_CMV_13(num_points, datapoints_1, parameters), "test_cmv_13: - ") # Tests when all points are the same
+        self.assertTrue( set_CMV_13(num_points, datapoints_2, parameters), "test_cmv_13: - ") # Tests when many points are the origin but one combination is outside radius1
+        self.assertTrue( set_CMV_13(num_points, datapoints_3, parameters), "test_cmv_13: - ") # Tests when two points are the same. Outside fo radius 1 but inside radius 2
+        self.assertFalse(set_CMV_13(num_points, datapoints_4, parameters), "test_cmv_13: - ") # Tests colinear points that are outside both radius 1 and radius2 
+    
+
+    def test_smallest_containting_circle(self):
+        points_1 = [(0,0),(1,0),(0,1)]
+        points_2 = [(-1,0),(1,0),(0,1)]
+        points_3 = [(-3,0),(-4,3),(-4,-3)]
+        points_4 = [(1,1),(3,3),(5,5)]
+        points_5 = [(-1,0),(3,0),(5,0)]
+        points_6 = [(1,1),(1,1),(1,1)]
+        points_7 = [(-4,-3),(-4,-3),(-4,3)]
+
+        # Test for points in Quadrant 1
+        center_1, radius_1 = smallest_containting_circle(points_1)
+        self.assertEqual(center_1,(0.5,0.5))
+        self.assertEqual(radius_1, 0.7071067811865476)
+
+        # Test for points on unit circle
+        center_2, radius_2 = smallest_containting_circle(points_2)
+        self.assertEqual(center_2,(0.0,0.0))
+        self.assertEqual(radius_2, 1.0)
+
+        # Test for negative points in Quadrants 2 and 3
+        center_3, radius_3 = smallest_containting_circle(points_3)
+        self.assertEqual(center_3,(-4.0,0.0))
+        self.assertEqual(radius_3, 3.0)
+
+        # Test for diagonal colinear points
+        center_4, radius_4 = smallest_containting_circle(points_4)
+        self.assertEqual(center_4,(3.0,3.0))
+        self.assertEqual(radius_4, 2.8284271247461903) 
+
+        # Test for horisontal colinear points
+        center_5, radius_5 = smallest_containting_circle(points_5)
+        self.assertEqual(center_5,(2.0,0.0))
+        self.assertEqual(radius_5, 3.0)
+
+        # Test when all three points are the same
+        center_6, radius_6 = smallest_containting_circle(points_6)
+        self.assertEqual(center_6,(1.0,1.0))
+        self.assertEqual(radius_6, 0)
+
+        # Test when all two points are the same
+        center_7, radius_7 = smallest_containting_circle(points_7)
+        self.assertEqual(center_7,(-4.0,0.0))
+        self.assertEqual(radius_7, 3.0)
+
+         
+       
 if __name__ == '__main__':
     unittest.main()
