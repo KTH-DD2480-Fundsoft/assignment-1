fix: replace angle calculation method from using np.arccos to math.atan2 (#85)
* fix: replace angle calculation method from using np.arccos to math.atan2

* fix: remove old evaluate_LIC_9 that was commented out

* fix: add tests with some negative values to test new angle calculation method in evaluate_LIC_2() and evaluate_LIC_9()

Resolves #85 

---------

Co-authored-by: sebastianmonten <94540990+sebastianmonten@users.noreply.github>

LIC_evaluation.py
@@ -95,22 +95,18 @@ def evaluate_LIC_2(num_points, datapoints, parameters):
     epsilon = parameters["epsilon"]
     angle_cond = False
     for i in range(num_points-2):
-        p1 = datapoints[i]
-        p2 = datapoints[i+1]
-        p3 = datapoints[i+2]
-        
+        vertex = datapoints[i+1]
+
         # Cannot form an angle if the first or third point is equal to the vertex
-        if np.array_equal(p1, p2) or np.array_equal(p3, p2):
+        # np.all(a == b) allows two np.ndarray a and b to be compared
+        if (np.all(datapoints[i] == vertex) or np.all(datapoints[i+2] == vertex)):
             continue
 
-        a = np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
-        b = np.sqrt((p3[0] - p2[0])**2 + (p3[1] - p2[1])**2)
-        c = np.sqrt((p3[0] - p1[0])**2 + (p3[1] - p1[1])**2)
+        first_ray = np.array(vertex) - np.array(datapoints[i])
+        second_ray = np.array(vertex) - np.array(datapoints[i+2])
 
-        # c^2 = a^2 + b^2 - 2abcos(angle p1p2p3)
-        # angle p1p2p3 = arccos((a^2 + b^2 - c^2) / (2ab)), see https://en.wikipedia.org/wiki/Law_of_cosines
-        angle = np.arccos((a**2 + b**2 - c**2) / (2 * a * b) )
-
+		# Taken from https://stackoverflow.com/questions/2827393/angles-between-two-n-dimensional-vectors-in-python
+        angle = math.atan2(np.linalg.det([second_ray, first_ray]), np.dot(second_ray, first_ray))
         if angle < np.pi - epsilon or angle > np.pi + epsilon:
             angle_cond = True
             break
@@ -291,11 +287,10 @@ def evaluate_LIC_8(num_points, datapoints, parameters):
 def evaluate_LIC_9(num_points, datapoints, parameters):
     cpts = parameters['cpts']
     dpts = parameters['dpts']
-    epsilon = parameters['epsilon']
+    epsilon = parameters["epsilon"]
 
     if num_points < 5:
           return False # "When NUMPOINTS < 5, the condition is not met"
-
     if not (1 <= cpts and 1 <= dpts):
           # TD: raise error here instead?
           return False # conditions not met for cpts and dpts
@@ -304,36 +299,27 @@ def evaluate_LIC_9(num_points, datapoints, parameters):
           return False # conditions not met for cpts and dpts
 
     # going through each possible set of three points and checking angle
+
+    angle_cond = False
     for i in range(0, num_points - cpts - dpts - 2):
-          # current three points, separated by cpts and dpts points respectively
-          p1 = datapoints[i]
-          p2 = datapoints[i+cpts+1]
-          p3 = datapoints[i+cpts+1+dpts+1]
+        p1     = datapoints[i]
+        vertex = datapoints[i+cpts+1]
+        p3     = datapoints[i+cpts+1+dpts+1]
 
-          if np.array_equal(p1,p2) or np.array_equal(p3, p2):
-                continue # "p1 and or p3 can not coincide with p2"
-          else:
-                
-                a = np.sqrt(
-                      (p1[0]-p2[0])**2 +
-                      (p1[1]-p2[1])**2
-                )
-                b = np.sqrt(
-                      (p3[0]-p2[0])**2 +
-                      (p3[1]-p2[1])**2
-                )
-                c = np.sqrt(
-                      (p3[0]-p1[0])**2 +
-                      (p3[1]-p1[1])**2
-                )
-                # c^2 = a^2 + b^2 - 2abcos(angle p1p2p3)
-                # angle p1p2p3 = arccos((a^2 + b^2 - c^2) / (2ab)), see https://en.wikipedia.org/wiki/Law_of_cosines
-                angle = np.arccos((a**2 + b**2 - c**2) / (2 * a * b) )
+        # Cannot form an angle if the first or third point is equal to the vertex
+        # np.all(a == b) allows two np.ndarray a and b to be compared
+        if (np.all(p1 == vertex) or np.all(p3 == vertex)):
+            continue
 
-                if ((angle < np.pi - epsilon) or (angle > np.pi + epsilon)):
-                      # angle p1p2p3 can not be in the range PI ± epsilon
-                      return True
-    return False
+        first_ray = np.array(vertex) - np.array(p1)
+        second_ray = np.array(vertex) - np.array(p3)
+
+		# Taken from https://stackoverflow.com/questions/2827393/angles-between-two-n-dimensional-vectors-in-python
+        angle = math.atan2(np.linalg.det([second_ray, first_ray]), np.dot(second_ray, first_ray))
+        if angle < np.pi - epsilon or angle > np.pi + epsilon:
+            angle_cond = True
+            break
+    return angle_cond
 
 def evaluate_LIC_10(num_points, datapoints, parameters):
     """