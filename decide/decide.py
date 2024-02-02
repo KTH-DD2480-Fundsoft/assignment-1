65662b8aad0f26135a778456c3bd8d9bc6e64d70
rasdan <rasdan@kth.se>
2024-02-02 16:09:23 +0100

    refactor: Remove dead code
    remove unused pum_not_contain_false_statement
    
    remove old comments

--- decide/decide.py
+++ decide/decide.py
@@ -173,7 +173,6 @@ def compute_PUM(cmv,puv,lcm):
     pum = [[True for _ in range(15)] for _ in range(15)]
     nr_of_rows, nr_of_cols = len(lcm), len(lcm[0])
     for i in range(nr_of_rows):
-        # TODO: Potential out of bounds in "puv[i]"?
         if puv[i]:
             for j in range(nr_of_cols):
                 if lcm[i][j] == CONNECTORS.ANDD:
@@ -243,90 +242,23 @@ def compute_cmv(lic_parameters, nr_of_data_points, points):
 
     cmv = [True] * NUMBER_OF_CONDITIONS
     
-    # cmv[0] = evaluate_lic_0(lic_parameters[0], nr_of_data_points, points)
     cmv[0] = evaluate_LIC_0(nr_of_data_points, points, lic_parameters)
-    
-    # cmv[1] = evaluate_lic_1(lic_parameters[1], nr_of_data_points, points)
     cmv[1] = evaluate_LIC_1(nr_of_data_points, points, lic_parameters)
-    
-    # cmv[2] = evaluate_lic_2(lic_parameters[2], nr_of_data_points, points)
     cmv[2] = evaluate_LIC_2(nr_of_data_points, points, lic_parameters)
-    
-    # cmv[3] = evaluate_lic_3(lic_parameters[3], nr_of_data_points, points)
     cmv[3] = evaluate_LIC_3(nr_of_data_points, points, lic_parameters)
-    
-    # cmv[4] = evaluate_lic_4(lic_parameters[4], nr_of_data_points, points)
     cmv[4] = evaluate_LIC_4(nr_of_data_points, points, lic_parameters)
-    
-    # cmv[5] = evaluate_lic_5(lic_parameters[5], nr_of_data_points, points)
     cmv[5] = evaluate_LIC_5(nr_of_data_points, points, lic_parameters)
-    
-    # cmv[6] = evaluate_lic_6(lic_parameters[6], nr_of_data_points, points)
     cmv[6] = evaluate_LIC_6(nr_of_data_points, points, lic_parameters)
-    
-    # cmv[7] = evaluate_lic_7(lic_parameters[7], nr_of_data_points, points)
     cmv[7] = evaluate_LIC_7(nr_of_data_points, points, lic_parameters)
-    
-    # cmv[8] = evaluate_lic_8(lic_parameters[8], nr_of_data_points, points)
     cmv[8] = evaluate_LIC_8(nr_of_data_points, points, lic_parameters)
-    
-    # cmv[9] = evaluate_lic_9(lic_parameters[9], nr_of_data_points, points)
     cmv[9] = evaluate_LIC_9(nr_of_data_points, points, lic_parameters)
-    
-    # cmv[10] = evaluate_lic_10(lic_parameters[10], nr_of_data_points, points)
     cmv[10] = evaluate_LIC_10(nr_of_data_points, points, lic_parameters)
-    
-    # cmv[11] = evaluate_lic_11(lic_parameters[11], nr_of_data_points, points)
     cmv[11] = evaluate_LIC_11(nr_of_data_points, points, lic_parameters)
-    
-    # cmv[12] = evaluate_lic_12(lic_parameters[12], nr_of_data_points, points)
     cmv[12] = evaluate_LIC_12(nr_of_data_points, points, lic_parameters)
-    
-    # cmv[13] = evaluate_lic_13(lic_parameters[13], nr_of_data_points, points)
     cmv[13] = evaluate_LIC_13(nr_of_data_points, points, lic_parameters)
-    
-    # cmv[14] = evaluate_lic_14(lic_parameters[14], nr_of_data_points, points)
     cmv[14] = evaluate_LIC_14(nr_of_data_points, points, lic_parameters)
 
     return cmv
-
-
-def does_pum_not_contain_false_element(lcm, cmv, puv):
-    """
-        Computes the truth values of the PUM and checks if there is a meaningful
-        false element in the PUM which will result in a false element in the FUV,
-        and consequently prevent a launch.
-
-
-        Parameters
-        ----------
-        lcm : (`matrix`)
-            The LCM.
-        cmv : (`vector`)
-            The CMV.
-        puv : (`vector`)
-            The PUV.
-
-        Returns
-        ----------
-        truth_value : (`Bool`)
-            True if PUM does not contain a false element which will prevent a launch,
-            otherwise False and launch should not occur.
-    """
-    nr_of_rows, nr_of_cols = len(lcm), len(lcm[0])
-    for i in range(nr_of_rows):
-        # TODO: Potential out of bounds in "puv[i]"?
-        if puv[i]:
-            for j in range(nr_of_cols):
-                if lcm[i][j] == CONNECTORS.ANDD:
-                    truth_value = cmv[i] and cmv[j]
-                    if truth_value == False:
-                        return False
-                elif lcm[i][j] == CONNECTORS.ORR:
-                    truth_value = cmv[i] or cmv[j]
-                    if truth_value == False:
-                        return False
-    return True
 
 
 
